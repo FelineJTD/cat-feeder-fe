53c5b6807d7c9ff6798eb3159773d399109c7916
FelineJTD <13520050@std.stei.itb.ac.id>
2024-03-25 17:19:01 +0700

    chore: create pages folder

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <p class="text-3xl font-bold underline">HELLO</p>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p class="text-3xl font-bold underline">HELLO</p>
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
