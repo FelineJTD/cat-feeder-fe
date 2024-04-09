25cb4bab5b7d421b877f5fc733473949ee5d224b
FelineJTD <13520050@std.stei.itb.ac.id>
2024-04-09 18:28:45 +0700

    feat: right decor

--- pages/cam/[idx].vue
+++ pages/cam/[idx].vue
@@ -7,32 +7,16 @@
   const numOfCams = 2
 
   const currCam = parseInt(route.params.idx)
-
-  onBeforeMount(() => {
-    // scroll to top
-    window.scrollTo(0, 0)
-  }),
-
-  onMounted(() => {
-    window.onscroll = function(e) {
-      if (this.oldScroll > this.scrollY) {
-        navigateTo('/cam/' + ((currCam - 2 + numOfCams) % numOfCams + 1))
-      } else {
-        navigateTo('/cam/' + (currCam % numOfCams + 1))
-      }
-      this.oldScroll = this.scrollY;
-    }
-  })
-
-
 </script>
 
 <template>
   <div>
     <img src="https://picsum.photos/1920/1081" alt="Background" class="fixed top-0 left-0 h-screen w-full object-cover -z-10" />
-    <div class="h-[calc(100vh+0.5px)] w-full flex flex-col justify-center p-24 bg-neutral-950/50">
+    <div class="h-screen w-full">
       <h1 class="text-3xl font-bold text-white">{{ $route.params.idx }}</h1>
       <NuxtLink to="/test" class="mt-4 px-4 py-2 bg-primary-500 text-white rounded-md">Liat Kucing</NuxtLink>
+      <!-- RIGHT DECOR -->
+      <div class="absolute right-32 top-0 border-r-2 hidden lg:block h-1/3" />
     </div>
   </div>
 </template>
