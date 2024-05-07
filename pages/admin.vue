76d58f888264917fbc11bd79956cfc6d5f95d998
FelineJTD <13520050@std.stei.itb.ac.id>
2024-05-07 04:55:13 +0200

    feat: charts from vue-notus

---
+++ pages/admin.vue
@@ -0,0 +1,11 @@
+<script setup>
+import CardLineChart from '~/components/Cards/CardLineChart.vue';
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-black">
+    <h1></h1>
+    <CardLineChart />
+  </div>
+</template>
